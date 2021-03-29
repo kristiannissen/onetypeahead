9cafa278b6e6fdafbf44e0e5bc350f6af6dc1724
Kristian Nissen <kristian.nissen@gmail.com>
2021-03-29 13:53:54 +0200

    Uglify

--- src/typeahead.js
+++ src/typeahead.js
@@ -14,30 +14,30 @@ var TypeAhead = (function () {
         if (state.hasOwnProperty(k) && state[k] !== v) {
             state[k] = v;
             // Dispatch Event
-            domElm.dispatchEvent(new CustomEvent('stateChanged', {
+            domElm.dispatchEvent(new CustomEvent("stateChanged", {
                 detail: {
-                    'key': k,
-                    'value': v
+                    "key": k,
+                    "value": v
                 }
             }));
         }
     };
     (function() {
         // Fetch products
-        fetch('/products.json')
+        fetch("/products.json")
             .then(function(resp) {
                 return resp.json();
             })
             .then(function(data) {
-                setState('products', data.products)
-            })
+                setState("products", data.products);
+            });
     })();
     // Render suggestions
     renderSuggestions = function(products) {
         var options = products.map(function(p) {
-            return '<div data-type="product" data-url="'+ p.url +'">'+ p.name +'</div>';
+            return `<div data-type="product" data-url="${p.url}">${p.name}</div>`;
         }); 
-        domElm.querySelector('#options').innerHTML = options.join('');
+        domElm.querySelector("#options").innerHTML = options.join("");
     };
     // Public methods
     return {
@@ -45,40 +45,45 @@ var TypeAhead = (function () {
         init: function(elm) {
             domElm = elm;
             // Append child container
-            var listElm = document.createElement('div');
-            listElm.setAttribute('id', 'options');
+            var listElm = document.createElement("div");
+            listElm.setAttribute("id", "options");
             domElm.appendChild(listElm);
 
-            domElm.addEventListener('stateChanged', function(evnt) {
-                if (evnt.detail.key == 'query') {
+            domElm.addEventListener("stateChanged", function(evnt) {
+                if (evnt.detail.key == "query") {
                     if (evnt.detail.value.length > 3) {
                         var products = state.products.filter(function(p) {
-                            return p.name.toLowerCase().includes(evnt.detail.value)
+                            return p.name.toLowerCase().includes(evnt.detail.value);
                         });
-                        listElm.innerHtml = '';
+                        listElm.innerHtml = "";
                         renderSuggestions(products);
                     } else {
-                        listElm.innerHTML = '';
+                        listElm.innerHTML = "";
                     }
                 }
             });
+            // Outside click handler
+            document.addEventListener("click", function(evnt) {
+                var t = evnt.target;
+                if (t !== listElm) listElm.innerHTML = "";
+            });
             // Click handler
-            domElm.addEventListener('click', function(evnt) {
+            domElm.addEventListener("click", function(evnt) {
                var elm = evnt.target;
-                if (elm.hasAttribute('data-type')) {
-                    document.location = '#!/products/'+ elm.getAttribute('data-url');
-                    listElm.innerHTML = '';
+                if (elm.hasAttribute("data-type")) {
+                    document.location = "#!/products/"+ elm.getAttribute("data-url");
+                    listElm.innerHTML = "";
                 }
             });
             // Event triggers
-            domElm.querySelector('#query')
-                .addEventListener('keyup', function(evnt) {
+            domElm.querySelector("#query")
+                .addEventListener("keyup", function(evnt) {
                     evnt.preventDefault();
                     // Change state
-                    setState('query', evnt.target.value.trim());
-                })
+                    setState("query", evnt.target.value.trim());
+                });
         },
     };
 })();
 //
-TypeAhead.init(document.querySelector('#typeahead'));
+TypeAhead.init(document.querySelector("#typeahead"));
