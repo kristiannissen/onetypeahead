a95ebff66cf7571fcd70dcaa4c474b2bf8bdf706
Kristian Nissen <kristian.nissen@gmail.com>
2021-09-12 10:24:02 +0200

    Updating dist

--- dist/typeahead.js
+++ dist/typeahead.js
@@ -1,1 +1,102 @@
-var TypeAhead=function(){var e={query:"",products:[]},t=null;return setState=function(n,r){e.hasOwnProperty(n)&&e[n]!==r&&(e[n]=r,t.dispatchEvent(new CustomEvent("stateChanged",{detail:{key:n,value:r}})))},fetch("/products.json").then(function(e){return e.json()}).then(function(e){setState("products",e.products)}),renderSuggestions=function(e){var n=e.map(function(e){return`<div data-type="product" data-url="${e.url}">${e.name}</div>`});t.querySelector("#options").innerHTML=n.join("")},{init:function(n){t=n;var r=document.createElement("div");r.setAttribute("id","options"),t.appendChild(r),t.addEventListener("stateChanged",function(t){if("query"==t.detail.key)if(t.detail.value.length>2){var n=e.products.filter(function(e){return e.name.toLowerCase().includes(t.detail.value)});r.innerHtml="",renderSuggestions(n)}else r.innerHTML=""}),document.addEventListener("click",function(e){e.target!==r&&(r.innerHTML="")}),t.addEventListener("click",function(e){var t=e.target;t.hasAttribute("data-type")&&(document.location="#!/products/"+t.getAttribute("data-url"),r.innerHTML="")}),t.querySelector("#query").addEventListener("keyup",function(e){e.preventDefault(),setState("query",e.target.value.trim())})}}}();TypeAhead.init(document.querySelector("#typeahead"));
+/**
+ * @file: typeahead.js
+ */
+var TypeAhead = (function () {
+    // The state contains the query entered by the user
+    // and the array of products
+    var state = {
+        query: "",
+        products: []
+    };
+    var domElm = null;
+    // Set state
+    var setState = function(k, v) {
+        if (state.hasOwnProperty(k) && state[k] !== v) {
+            state[k] = v;
+            // Dispatch Event
+            domElm.dispatchEvent(new CustomEvent("stateChanged", {
+                detail: {
+                    "key": k,
+                    "value": v
+                }
+            }));
+        }
+    };
+    (function() {
+        // Fetch products
+        fetch("/products.json")
+            .then(function(resp) {
+                return resp.json();
+            })
+            .then(function(data) {
+                setState("products", data.products);
+            });
+    })();
+    // Render suggestions
+    renderSuggestions = function(products) {
+        var options = products.map(function(p) {
+            return `<div data-url="${p.url}">${p.name}</div>`;
+        }); 
+        domElm.querySelector("#options").innerHTML = options.join("");
+    };
+    // Public methods
+    return {
+        // TypeAhead.init(documentElement)
+        init: function(elm) {
+            domElm = elm;
+            // Append child container
+            var listElm = document.createElement("div");
+            listElm.setAttribute("id", "options");
+            domElm.appendChild(listElm);
+
+            domElm.addEventListener("stateChanged", function(evnt) {
+                if (evnt.detail.key == "query") {
+                    if (evnt.detail.value.length > 2) {
+                        var products = state.products.filter(function(p) {
+                            return p.name.toLowerCase().includes(evnt.detail.value.toLowerCase());
+                        });
+                        listElm.innerHtml = "";
+                        renderSuggestions(products);
+                        listElm.style = "visibility: visible;";
+                    } else {
+                        listElm.innerHTML = "";
+                        listElm.style = "visibility: hidden;";
+                    }
+                }
+            });
+            // Outside click handler
+            document.addEventListener("click", function(evnt) {
+                var t = evnt.target;
+                if (t !== listElm) listElm.innerHTML = "";
+            });
+            // Click handler
+            domElm.addEventListener("click", function(evnt) {
+               var elm = evnt.target;
+                if (elm.hasAttribute("data-type")) {
+                    // window.history.pushState({}, "", "#!/products/"+ elm.getAttribute("data-url"));
+                    document.location.hash = "#!/products/"+ elm.getAttribute("data-url");
+                    listElm.innerHTML = "";
+                }
+            });
+            // Event triggers
+            domElm.querySelector("#query")
+                .addEventListener("keyup", function(evnt) {
+                    evnt.preventDefault();
+                    // Change state
+                    setState("query", evnt.target.value.trim());
+                });
+            // Toggle bottom border on focus
+            domElm.querySelector("#query")
+                .addEventListener("focus", function(evnt) {
+                    evnt.target.style = "border-bottom: 1px solid rgb(237, 100, 47);";
+                });
+            // Toggle bottom border on blur
+            domElm.querySelector("#query")
+                .addEventListener("blur", function(evnt) {
+                    evnt.target.style = "border-bottom: 1px solid #000;"
+                });
+        },
+    };
+})();
+//
+TypeAhead.init(document.querySelector("#typeahead"));
